d08eef2172e1f73c5862f55f26f82ce05e1c2869
takemura <fumiaki.takemura@rakus-partners.co.jp>
2025-01-20 10:48:55 +0900

    first commit

---
+++ src/Ex02.java
@@ -0,0 +1,10 @@
+public class Ex02 {
+
+    public static void main(String[] args) {
+        String message = "I have a dream today";
+        int intMessage = message.indexOf("a");
+        System.out.println("最初のaは"+intMessage+"番目です");
+    }
+    
+
+}
---
+++ src/Ex03.java
@@ -0,0 +1,14 @@
+public class Ex03 {
+    public static void main(String[] args) {
+        String message1 = "I have a dream today";
+        String message2 = "I have an dream today";
+
+        if(message1.equals(message2)){
+            System.out.println("文字列が同じです");
+        }else{
+            System.out.println("文字列が同じではありません");
+        }
+
+    }
+
+}
